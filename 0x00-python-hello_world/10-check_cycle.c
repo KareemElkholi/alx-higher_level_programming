2cbfd84615295b4afe49bc2276ba01d21088cc49
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-10-31 05:18:17 +0200

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list is cyclical
+ * @list: pointer to list
+ * Return: 1 o 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	while (fast && fast->next)
+	{
+		slow = list->next;
+		fast = list->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
