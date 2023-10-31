d99f59235ab97a22790f3c67863e3c8638d2953c
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-10-31 06:35:37 +0200

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -10,14 +8,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	while (fast && fast->next)
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
 	{
-		slow = list->next;
-		fast = list->next->next;
+		slow = slow->next;
+		fast = fast->next->next;
 		if (slow == fast)
 			return (1);
 	}
+
 	return (0);
 }
