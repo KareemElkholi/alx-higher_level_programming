d6505e64e4d453863fa3b01cad3f51ed857ba54d
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-11-02 22:02:42 +0200

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        a = sys.argv[1]
-        b = sys.argv[3]
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         op = sys.argv[2]
+        print(ops[op])
         print("{} {} {} = {}".format(a, op, b, ops[op](a, b)))
