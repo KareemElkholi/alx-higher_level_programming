a5590a572acf2e00192a2eac53b3760406e67c08
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-11-11 04:19:13 +0200

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str):
+        n = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = i = 0
+        length = len(roman_string)
+        while i < length:
+            if i == length-1 or n[roman_string[i]] >= n[roman_string[i+1]]:
+                res += n[roman_string[i]]
+                i += 1
+            else:
+                res = n[roman_string[i+1]] - n[roman_string[i]]
+                i += 2
+        return res
+    else:
+        return 0
