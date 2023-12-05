72530f460892c8d61cec6d1139afbb4f698cf81b
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-12-05 02:52:14 +0200

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,8 +6,3 @@ class MyList(list):
     """def"""
     def print_sorted(self):
         print(sorted(self))
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/1-my_list.txt")
