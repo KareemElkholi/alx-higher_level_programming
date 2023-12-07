e35683baee182d12ddfd55ae5d73f4123aa1f482
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-12-07 03:36:45 +0200

    10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
         self.__size = size
-        self.integer_validator("size", self.__size)
+        self.integer_validator("size", size)
 
     def area(self):
         return self.__size ** 2
